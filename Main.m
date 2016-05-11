9c0a26a83888fce1af3a0d33564cbca3983b4229
Martin Hallén <marthall@outlook.com>
2016-05-11 11:56:00 +0200

    init

---
+++ Main.m
@@ -0,0 +1,58 @@
+
+NUM_VERTICES = 50;
+C = 10;
+Q = 4;
+N_steps = 1000;
+B = 0.1;
+
+cost_array = zeros(N_steps, 1);
+
+x = randi(Q, NUM_VERTICES, 1);
+
+A = zeros(NUM_VERTICES);
+
+for i = 1:NUM_VERTICES
+    for j = i+1:NUM_VERTICES
+        if rand() < C / NUM_VERTICES
+            A(i, j) = 1;
+            A(j, i) = 1;
+        end
+    end
+   
+end
+
+cost_function(A, x)
+
+G = graph(A);
+
+G.Nodes.NodeColors = x;
+
+P = plot(G, 'MarkerSize', 12);
+
+P.NodeCData = G.Nodes.NodeColors;
+
+for t=1:N_steps
+%     Copy old color list
+    new_x = x;
+    vertex = randi(NUM_VERTICES);
+    
+%     Select new color
+    old_color = x(vertex);
+    while old_color == new_x(vertex)
+        new_x(vertex) = randi(Q);
+    end
+    
+    cost = cost_function(A, x);
+    new_cost = cost_function(A, new_x);
+    
+    cost_array(t) = cost;
+    
+    delta_H = new_cost - cost;
+    
+    if delta_H <= 0 || rand() > exp(-B * delta_H)
+        x = new_x;
+    end
+end
+
+plot(cost_array);
+
---
+++ cost_function.m
@@ -0,0 +1,18 @@
+function [ cost ] = cost_function(A, x)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+cost = 0;
+[h,w] = size(A);
+
+for vertex = 1:h
+    color = x(vertex);
+    row = A(vertex, :);
+    matching_colors = x(logical(row));
+    
+    cost = cost + nnz(matching_colors==color);
+
+end
+
+cost = cost / 2;
+
